feec96dbd86ecd59e3df3b50b9aa22dccefec8ce
Sobhasri05 <147487773+Sobhasri05@users.noreply.github.com>
2023-12-05 15:32:13 +0530

    #include<stdio.h> int main() {     int first=0,second=1,third,i,n,flag=0;     scanf("%d",&n);     third=first+second;     while(third<=n)     {         if(third==n)         {             flag=1;             break;         }         //13         first=second;         second=third;         third=first+second;     }     if(flag==1)     {         printf("True");     }     else     {         printf("False");     } }

---
+++ Fibonacci_or_not.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int first=0,second=1,third,i,n,flag=0;
+    scanf("%d",&n);
+    third=first+second;
+    while(third<=n)
+    {
+        if(third==n)
+        {
+            flag=1;
+            break;
+        }
+        //13
+        first=second;
+        second=third;
+        third=first+second;
+    }
+    if(flag==1)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}
